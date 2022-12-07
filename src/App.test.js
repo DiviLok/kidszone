17c98b68665a1049a0f4a355ac681e7ad4255020
Divya Jagannatha <dija0002@student.hv.se>
2022-12-07 22:31:29 +0100

    Add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-
+test('renders kidzzy link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Kidzzy/i);
+  expect(linkElement).toBeInTheDocument();
+});
